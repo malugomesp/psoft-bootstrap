c1e1f9dcc948383778afba43b213e8a449d11d35
malugomesp <malu.gomes439@gmail.com>
2026-09-24 18:43:04 -0300

    resolucao atv1.2

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,8 +1,27 @@
-public class Cliente extends Pessoa {
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private Perfil tipoPerfil;
 
-    public Cliente(String nome, String cpf, int idade, String telefone, String profissao) {
-        super(nome, cpf, idade, telefone, profissao);
-        //TODO Auto-generated constructor stub
+    public Cliente(String cpf, String nome, Perfil tipoPerfil) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.tipoPerfil = tipoPerfil;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setTipoPerfil(Perfil perfil){
+        this.tipoPerfil = perfil;
     }
     
+    public double calculaDesconto(){
+        return this.tipoPerfil.getDesconto();
+    }
 }
